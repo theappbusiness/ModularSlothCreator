c4baf227350d913cd16ac80ba85c472d1d4f4e5b
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-04-12 17:29:08 +0100

    Add Viewing framework

---
+++ ModularSlothCreator/Viewing/Viewing.h
@@ -0,0 +1,18 @@
+//
+//  Viewing.h
+//  Viewing
+//
+//  Created by Pranav Kasetti on 12/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Viewing.
+FOUNDATION_EXPORT double ViewingVersionNumber;
+
+//! Project version string for Viewing.
+FOUNDATION_EXPORT const unsigned char ViewingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Viewing/PublicHeader.h>
+
+
