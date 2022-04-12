63b5a55df1df1da19d650e46ee0bd38898679fac
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-04-12 17:25:53 +0100

    Add Care framework

---
+++ ModularSlothCreator/Care/Care.h
@@ -0,0 +1,18 @@
+//
+//  Care.h
+//  Care
+//
+//  Created by Pranav Kasetti on 12/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Care.
+FOUNDATION_EXPORT double CareVersionNumber;
+
+//! Project version string for Care.
+FOUNDATION_EXPORT const unsigned char CareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Care/PublicHeader.h>
+
+
