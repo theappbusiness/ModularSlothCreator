8fdfae27c5d0098f990533965463a28dd380fd04
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-04-12 17:23:16 +0100

    Initial commit

---
+++ ModularSlothCreator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModularSlothCreator
+//
+//  Created by Pranav Kasetti on 12/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModularSlothCreator/ModularSlothCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularSlothCreatorApp.swift
+//  ModularSlothCreator
+//
+//  Created by Pranav Kasetti on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ModularSlothCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
