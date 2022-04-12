3ae4490f339da805a6057997e09f75828a12eefd
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-04-12 17:24:14 +0100

    Add Creation framework

---
+++ ModularSlothCreator/Creation/Creation.h
@@ -0,0 +1,18 @@
+//
+//  Creation.h
+//  Creation
+//
+//  Created by Pranav Kasetti on 12/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Creation.
+FOUNDATION_EXPORT double CreationVersionNumber;
+
+//! Project version string for Creation.
+FOUNDATION_EXPORT const unsigned char CreationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Creation/PublicHeader.h>
+
+
