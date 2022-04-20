a5437f7e428de502991a904d0d822952d62f7d82
Pranav Kasetti <pranav.kasetti@kinandcarta.com>
2022-04-20 12:18:51 +0100

    Update ContentView with Sloth.

--- ModularSlothCreator/ContentView.swift
+++ ModularSlothCreator/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import Viewing
+import Models
 
 struct ContentView: View {
+    @State var sloth: Sloth = .init(name: "SuperSloth",
+                                    color: .blue,
+                                    power: .ice)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SlothView(sloth: $sloth)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
+    @State static var sloth: Sloth = .init(name: "SuperSloth", color: .blue, power: .ice)
+
     static var previews: some View {
-        ContentView()
+        SlothView(sloth: $sloth)
     }
 }
